c1fdef69062d91e1191525c11a99c40e6011a931
Dustin Updyke <43444464+sei-dupdyke@users.noreply.github.com>
2025-11-24 16:49:34 -0500

    Version updates (#40)
    * Upgrades projects to .NET 10; updates most dependencies to current versions
    
    * Updates CodeQL to Crucible latest
    
    * Removes specific references to libraries that exist elsewhere for swagger and openapi
    
    * updates postgresql driver to version 10

---
+++ Gallery.Api.Migrations.PostgreSQL/Migrations/20251113194253_Version10UpgradeSync.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gallery.Api.Migrations.PostgreSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Version10UpgradeSync : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "exhibit_roles",
+                keyColumn: "id",
+                keyValue: new Guid("f870d8ee-7332-4f7f-8ee0-63bd07cfd7e4"),
+                column: "description",
+                value: "Can edit the Exhibit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "exhibit_roles",
+                keyColumn: "id",
+                keyValue: new Guid("f870d8ee-7332-4f7f-8ee0-63bd07cfd7e4"),
+                column: "description",
+                value: "Has read only access to the Exhibit");
+        }
+    }
+}
--- Gallery.Api/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Gallery.Api/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -3,7 +3,6 @@
 
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.CodeAnalysis;
-using Microsoft.OpenApi.Models;
 using Gallery.Api.Infrastructure.Options;
 using Gallery.Api.Infrastructure.OperationFilters;
 using System;
@@ -11,6 +10,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text.Json;
+using Microsoft.OpenApi;
 
 namespace Gallery.Api.Infrastructure.Extensions
 {
@@ -44,27 +44,31 @@ namespace Gallery.Api.Infrastructure.Extensions
                     }
                 });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                c.AddSecurityRequirement((document) => new OpenApiSecurityRequirement
                 {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "oauth2"
-                            },
-                            Scheme = "oauth2"
-                        },
-                        new[] {authOptions.AuthorizationScope}
-                    }
+                    { new OpenApiSecuritySchemeReference("oauth2", document), [authOptions.AuthorizationScope] }
                 });
 
                 c.IncludeXmlComments(commentsFile);
                 c.EnableAnnotations();
                 c.OperationFilter<DefaultResponseOperationFilter>();
-                c.MapType<Optional<Guid?>>(() => new OpenApiSchema { Type = "string", Format = "uuid", Nullable = true });
-                c.MapType<JsonElement?>(() => new OpenApiSchema { Type = "object", Nullable = true });
+                c.MapType<Optional<Guid?>>(() => new OpenApiSchema
+                {
+                    OneOf = new List<IOpenApiSchema>
+                    {
+                        new OpenApiSchema { Type = JsonSchemaType.String, Format = "uuid" },
+                        new OpenApiSchema { Type = JsonSchemaType.Null }
+                    }
+                });
+
+                c.MapType<JsonElement?>(() => new OpenApiSchema
+                {
+                    OneOf = new List<IOpenApiSchema>
+                    {
+                        new OpenApiSchema { Type = JsonSchemaType.Object },
+                        new OpenApiSchema { Type = JsonSchemaType.Null }
+                    }
+                });
             });
         }
 
