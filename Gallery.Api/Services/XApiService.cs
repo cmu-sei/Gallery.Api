7a3f3bc4e5d6571ee9f014a90317b4dcc4103494
Adam Welle <arwelle@cert.org>
2026-05-06 19:00:03 +0000

    Add Enabled flag to XApiOptions to explicitly disable xAPI tracking

--- Gallery.Api/Services/XApiService.cs
+++ Gallery.Api/Services/XApiService.cs
@@ -105,7 +105,7 @@ namespace Gallery.Api.Services
 
         public Boolean IsConfigured()
         {
-            return !string.IsNullOrWhiteSpace(_xApiOptions.Username);
+            return _xApiOptions.Enabled && !string.IsNullOrWhiteSpace(_xApiOptions.Username);
         }
 
         public async Task<Boolean> ArticleViewedAsync(Exhibit exhibit, Article article, Card card, Collection collection, CancellationToken ct)
